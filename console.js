a406c2108bf181ec8d2e4f16431928d42e15540e
Christopher Bebry <invalidsyntax@gmail.com>
2013-04-28 12:13:33 -0400

    Initial commit.

---
+++ console.js
@@ -0,0 +1,21 @@
+  var Console = function(consoleObj,consoleLineObj) {
+	var console = $(consoleObj);
+	var consoleLine = $(consoleLineObj);
+	return {
+		WriteLine: function(data) {
+			var cin = this.ReadLine();
+			consoleLine.detach();
+			var consoleData = console.html();
+			console.html(consoleData + "<span class = 'pointToResponse'>></span>" + "<span class = 'dataIn'>" + cin + "</span>" + "<br><br>" + data + " " + consoleLine[0].outerHTML);
+			consoleLine = $(consoleLineObj);					
+		},
+		ReadLine: function() {
+			var data = consoleLine.val().trim();
+			return data;
+		},
+		Focus: function() {
+			consoleLine.focus();
+		}
+	}
+  
+  }
