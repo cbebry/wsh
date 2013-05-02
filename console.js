1fbb04f9b23bd54ea57c5968dba46ff1e33db427
Christopher Bebry <invalidsyntax@gmail.com>
2013-05-01 21:32:15 -0400

    Fixing up some styles.

--- console.js
+++ console.js
@@ -1,12 +1,15 @@
-  var Console = function(consoleObj,consoleLineObj) {
+var Console = function(consoleObj,consoleLineObj) {
 	var console = $(consoleObj);
 	var consoleLine = $(consoleLineObj);
 	return {
+		Initialize: function() {
+		
+		},
 		WriteLine: function(data) {
 			var cin = this.ReadLine();
 			consoleLine.detach();
 			var consoleData = console.html();
-			console.html(consoleData + "<span class = 'pointToResponse'>></span>" + "<span class = 'dataIn'>" + cin + "</span>" + "<br><br>" + data + " " + consoleLine[0].outerHTML);
+			console.html(consoleData + "<span class='pointToResponse'>></span>" + "<span class = 'dataIn'>" + cin + "</span>" + "<br><br>" + data + " " + consoleLine[0].outerHTML);
 			consoleLine = $(consoleLineObj);					
 		},
 		ReadLine: function() {
@@ -17,5 +20,4 @@
 			consoleLine.focus();
 		}
 	}
-  
-  }
+}
